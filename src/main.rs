b7fb9b87db011d74414d0b18b39a339755702d06
Michael Pusl <pusl03@ads.uni-passau.de>
2022-04-11 10:55:53 +0200

    Refactor: main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::io::Read;
-
 use crate::compile::Parser;
 use crate::scanner::Scanner;
 use crate::vm::VM;
@@ -14,29 +12,20 @@ mod vm;
 
 fn main() {
     let mut args = std::env::args();
+    args.next();
     if let Some(input) = args.next() {
-        let path = std::path::Path::new(&input);
-        if let Ok(mut file) = std::fs::File::open(path) {
-            let mut code = String::new();
-            if file.read_to_string(&mut code).is_ok() {
-                let chars = code.chars().collect::<Vec<char>>();
-                let scanner = Scanner::new(chars.as_slice());
-                let compiler = Parser::new(scanner.parse());
-                if let Ok((function, symbol_table)) = compiler.compile() {
-                    let vm = VM::new(function, symbol_table);
-                    if vm.interpret().is_ok() {
-                        println!("Success.");
-                    } else {
-                        println!("Vm error failed.");
-                    }
-                } else {
-                    println!("Compilation failed.");
-                }
+        if let Ok(code) = std::fs::read_to_string(input.clone()) {
+            let chars = code.chars().collect::<Vec<char>>();
+            let scanner = Scanner::new(chars.as_slice());
+            let compiler = Parser::new(scanner.parse());
+            if let Ok((function, symbol_table)) = compiler.compile() {
+                let vm = VM::new(function, symbol_table);
+                let _ = vm.interpret();
             } else {
-                println!("Cannot read file.");
+                println!("Compilation failed");
             }
         } else {
-            println!("Cannot open file.");
+            println!("Cannot read file '{}'", input);
         }
     } else {
         println!("Usage: rlox <path-to-lox-file>")
