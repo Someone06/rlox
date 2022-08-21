8581c3a0334f8f30344098fb7f49339caf20b8d2
Michael Pusl <pusl03@ads.uni-passau.de>
2022-08-21 09:46:13 +0200

    Refactor: Cleaned up test_generator macro.

--- libs/test_generator/src/lib.rs
+++ libs/test_generator/src/lib.rs
@@ -1,33 +1,60 @@
-use proc_macro::TokenStream;
-use quote::{format_ident, quote};
-use syn::{parse, LitStr};
+fn input_to_glob_pattern(input: proc_macro::TokenStream) -> String {
+    let mut input = syn::parse::<syn::LitStr>(input)
+        .expect("Could not parse make_test! input.")
+        .value();
+
+    if !input.ends_with('/') {
+        input.push('/');
+    }
+    input.push_str("*/*.lox");
+    input
+}
+
+fn glob_to_function_name_and_path(path: &glob::GlobResult) -> (String, String) {
+    let path = path
+        .as_ref()
+        .expect("Globbing the target directory should not fail.")
+        .as_path();
+    let file_name = path
+        .file_stem()
+        .expect("We only glob *.lox files, so there has to be a file step.")
+        .to_str()
+        .expect("Turning the OsString into a utf-8 string should not fail.");
+    let dir_name = path
+        .parent()
+        .expect("We only glob sub-directoires, so there should be a parent directory.")
+        .file_name()
+        .expect("We globed the sub-directory of a sub-directory so, the path does not terminate in '..'.")
+        .to_str()
+        .expect("Turning the OsString into a utf-8 string should not fail.");
+    let path = path
+        .to_str()
+        .expect("Turning the OsString into a utf-8 string should not fail.")
+        .to_string();
+    let function_name = format!("{}_{}", dir_name, file_name);
+    (function_name, path)
+}
+
+fn glob_pattern_to_function_name_and_path(pattern: &str) -> Vec<(String, String)> {
+    let mut functions = glob::glob(pattern)
+        .expect("Glob pattern should be correct if the given input is a path.")
+        .map(|glob_res| glob_to_function_name_and_path(&glob_res))
+        .collect::<Vec<(String, String)>>();
+    functions.sort();
+    functions
+}
 
 #[proc_macro]
-pub fn make_tests(input: TokenStream) -> TokenStream {
-    let input = parse::<LitStr>(input).expect("Could not parse make_test! input.");
-    let path = input.value();
-    let files = glob::glob(format!("{}{}", path, "/*/*.lox").as_str()).unwrap();
+pub fn make_tests(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let pattern = input_to_glob_pattern(input);
+    let functions = glob_pattern_to_function_name_and_path(pattern.as_str());
 
-    let mut vec: Vec<(String, String)> = Vec::new();
-    for path in files {
-        let path = path.unwrap();
-        let file_name = path.file_stem().unwrap().to_str().unwrap();
-        let dir_name = path
-            .parent()
-            .unwrap()
-            .file_name()
-            .unwrap()
-            .to_str()
-            .unwrap();
-        let path = path.to_str().unwrap().to_string();
-        let function_name = format!("{}_{}", dir_name, file_name);
-        vec.push((function_name, path));
-    }
+    let name = functions
+        .iter()
+        .map(|s| quote::format_ident!("{}", s.0.as_str()));
+    let path = functions.iter().map(|s| s.1.as_str());
 
-    let name = vec.iter().map(|s| format_ident!("{}", s.0.as_str()));
-    let path = vec.iter().map(|s| s.1.as_str());
-
-    let res = quote! {
+    let res = quote::quote! {
         #(
             #[test]
             fn #name() {
